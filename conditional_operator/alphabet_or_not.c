57d2a3955c47673abd56c014994cd76ccc83245d
priyanshinamdev123 <92726740+priyanshinamdev123@users.noreply.github.com>
2022-05-30 22:03:44 +0530

    Create alphabet_or_not.c
    C program to check alphabets using Conditional operator

---
+++ conditional_operator/alphabet_or_not.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+    (ch>='a' && ch<='z') || (ch>='A' && ch<='Z') 
+        ? printf("Alphabet")
+        : printf("Not alphabet");
+
+    return 0;
+}
