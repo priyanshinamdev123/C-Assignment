5b9351912656111df73ff5ce1db52050218212d4
priyanshinamdev123 <92726740+priyanshinamdev123@users.noreply.github.com>
2022-05-30 16:47:12 +0530

    Update even_odd.c

--- If-Else/even_odd.c
+++ If-Else/even_odd.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main() {
     int num;
-    printf("Enter an integer: ");
+    printf("Enter an number: ");
     scanf("%d", &num);
-
-    // true if num is perfectly divisible by 2
     if(num % 2 == 0)
         printf("%d is even.", num);
     else
