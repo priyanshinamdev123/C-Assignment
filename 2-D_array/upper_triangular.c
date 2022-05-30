87067c148ff96bc2074ad33d7442cf661f80a4af
priyanshinamdev123 <92726740+priyanshinamdev123@users.noreply.github.com>
2022-05-30 20:47:27 +0530

    Create upper_triangular.c
    C program to find upper triangular matrix

---
+++ 2-D_array/upper_triangular.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int i,n,j,sum = 0;
+    printf("Enter the size of an array: ");
+    scanf("%d",&n);
+    int a[n][n];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i>j)
+            {
+                a[i][j]=0;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
